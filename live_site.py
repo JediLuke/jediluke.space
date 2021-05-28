3142f0bc1e90df26241b14e715f97379d97c8908
Mary Grace Gloria <marygrace.gloria08@gmail.com>
2021-05-28 22:51:46 +0800

    Updates take-down command.

--- live_site.py
+++ live_site.py
@@ -1,4 +1,5 @@
 import os
+import json
 import click
 import requests
 import colorama
@@ -6,90 +7,114 @@ import digitalocean
 from colorama import Fore
 from colorama import init
 
-
-@click.command()
-@click.option('--status',default='http://jediluke.space',help='Check the status of your website.')
-@click.option('--take-down',default='jediluke-space',help='Deletes the droplet and remove the jediluke.space domain to droplet IP mapping.')
-def live(status, take_down):
-    init(autoreset=True)
-    response =''
-
-    try:    
-        response = requests.get(status, timeout=5)
-    except Exception as e:
-        click.echo(Fore.RED + ' Your website {} is down! - \n {}'.format(status,str(e)))    
-    else:
-        if response.status_code == 200:
-            click.echo(Fore.GREEN + ' HTTP status code: {} [Your website {} is up and running!]'.format(response.status_code,status))   
-        else:
-            click.echo(Fore.RED + ' HTTP status code: {} [Your website {} is down!]'.format(response.status_code,status))
-
-    domain_name = 'jediluke.space'
-    droplet_name = 'jediluke-space'
-    api_token = ''
-
-    api_url_base = 'https://api.digitalocean.com/v2/'
-
-    headers = {'Content-Type': 'application/json','Authorization': 'Bearer {0}'.format(api_token)}
-
-    if os.getenv('DIGITAL_OCEAN_AUTH_TOKEN'):
-        api_token = os.getenv('DIGITAL_OCEAN_AUTH_TOKEN')
-    else:
-        api_token = input ('Enter your API token: ')
-
-    def destroy(take_down):            
-
-        def get_all_droplets(api_url_base, headers):
-            api_url = '{0}droplets'.format(api_url_base)
-            response = requests.get(api_url, headers=headers)
-
-            if response.status_code == 200:
-                return json.loads(response.content.decode('utf-8'))
-            else:
-                return None
+if os.getenv('DIGITAL_OCEAN_AUTH_TOKEN'):
+	api_token = os.getenv('DIGITAL_OCEAN_AUTH_TOKEN')
+else:
+	api_token = input ('Enter your API token: ')
+#The api_url_base variable is the string that starts off every URL in the DigitalOcean API.
+api_url_base = 'https://api.digitalocean.com/v2/'
+#We need to set up the HTTP request headers the way the API docs describe.
+#Add these lines to the file to set up a dictionary containing your request headers:
+headers = {'Content-Type': 'application/json',
+		   'Authorization': 'Bearer {0}'.format(api_token)}
 
 
-        def del_droplet(api_token, droplet_id):
-            droplet = digitalocean.Droplet(token=api_token,
-                id=droplet_id)
+def get_all_droplets():
+	api_url = '{0}droplets'.format(api_url_base)
+	response = requests.get(api_url, headers=headers)
 
-            destroyed = droplet.destroy()
-            if destroyed == True:
-                click.echo(" Success! Your droplet with a '{}' ID has been deleted.".format(droplet_id))
-            else:
-                click.echo(' [!] Request failed!')
+	if response.status_code == 200:
+		return json.loads(response.content.decode('utf-8'))
+	else:
+		return 
 
-        def del_domain(domain_name):    
-            domain = digitalocean.Domain(token=api_token, name=domain_name)
+def del_droplet(droplet, dropletId):
+	droplet = digitalocean.Droplet(token=api_token,id=dropletId)
 
-            domain.destroy()
+	destroyed = droplet.destroy()
+	click.echo(" Success! Your droplet {} with a '{}' ID has been deleted.".format(droplet,dropletId))
+	
+def del_domain(domain_name):    
+	domain = digitalocean.Domain(token=api_token, name=domain_name)
 
-            click.echo(" Success! '{}' has been deleted.".format(domain_name))      
+	destroyed = domain.destroy()
 
-        def check_droplet(api_url_base, headers):
-            my_droplets = get_all_droplets(api_url_base, headers)
-            droplet_exists = ''
-            droplet_id: ''
+	if destroyed == True:
+		click.echo(" Success! Your domain name '{}' has been deleted.".format(domain_name))
+	else:   
+		click.echo(' [!] Request failed!')
 
-            if my_droplets is not None:
-             # check if the droplet already exists or not
-                #print(' Your droplets: ')
-                for droplet, details in enumerate(my_droplets['droplets']):
-                    #print('  * Droplet name: ',details['name'])
-                    for key, value in details.items():
-                        x = details['name']
-                        if droplet_name != x: continue
-                        click.echo('Droplet already exists!')
-                        droplet_exists = x
+def check_droplet(droplet):
+	my_droplets = get_all_droplets()
+	droplet_exists = ''
+	droplet_id: ''
+	
+	# check if the droplet already exists or not
+	for droplet, details in enumerate(my_droplets['droplets']):
+		for key, value in details.items():
+			x = details['name']
+			if droplet != x: continue
+			droplet_exists = x
 
-            # if domain exists, prompt to delete or not...
-            if droplet_exists == 'jediluke-space':
-                droplet_id = details['id']
-                #print('  * Droplet ID: ', droplet_id)
-                click.echo('>> Deleting your droplet {}... '.format(droplet_name))
-                del_droplet(api_token, droplet_id)
-                del_domain(domain_name)                                     
+	if droplet_exists:
+		droplet_id = details['id']
+		click.echo(droplet_id)
+		return droplet_id
 
-        
-if __name__ == "__main__":       
-    live()
+@click.group()
+@click.version_option(version='1.0', prog_name='Live')
+def main():
+	"""
+	This tool helps you:
+
+	1. Check website status.
+	2. Delete a droplet. 
+	3. Delete a domain name connected to a droplet.
+
+	Use help [OPTIONS] to see how to use each command.
+	Ex. live_site.py take-down --help
+	"""
+	pass        
+
+
+@main.command()
+@click.option('--url', '-u', default='http://jediluke.space', help='URL of the website.', prompt=True)
+#Check status of the website
+def status(url):
+	"""Check the status of a website by providing a URL.
+
+		Ex.
+		live_site.py status -u http://jediluke.space
+	"""
+	init(autoreset=True)
+	response =''
+
+	try:    
+		response = requests.get(url, timeout=5)
+		if response.status_code == 200:
+			click.echo(Fore.GREEN + ' HTTP status code: {} [Your website {} is up and running!]'.format(response.status_code,url))   
+		else:
+			click.echo(Fore.RED + ' HTTP status code: {} [Your website {} is down!]'.format(response.status_code,url))
+
+	except Exception as e:
+		click.echo(Fore.RED + ' Your website {} is down! - \n {}'.format(url,str(e)))    
+	
+
+@main.command()
+#Take down the droplet
+@click.option('--droplet', '-d', default='jediluke-space', help='Droplet name.', prompt=True)
+@click.option('--domain-name', '-dn', default='jediluke.space', help='Domain name.', prompt=True)
+def take_down(droplet, domain_name):
+	"""Take down or destroy a droplet. Provide a droplet name and domanin name attached to the droplet."""
+
+	dropletId = check_droplet(droplet)
+	click.echo(dropletId)
+	#click.echo('>> Deleting your droplet {} with droplet id {}... '.format(droplet, dropletId))
+	#del_droplet(droplet, dropletId)
+	#click.echo('>> Deleting your domain name {}... '.format(domain_name))
+	#del_domain(domain_name)
+
+
+if __name__ == '__main__':
+	main()
+
